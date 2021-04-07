9f26ca413816eb12be8b3e7e2ae71c4bbee8e47d
xmasterg <t.zvirgzdins@gmail.com>
2021-04-07 15:31:37 +0300

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,45 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+
+   <div class="row">
+        <div class="col-3 p-5">
+            <img src="https://instagram.frix3-1.fna.fbcdn.net/v/t51.2885-19/s150x150/38422525_644323985961269_544127499764760576_n.jpg?tp=1&_nc_ht=instagram.frix3-1.fna.fbcdn.net&_nc_ohc=SPiyxEUj2doAX9QVkEC&ccb=7-4&oh=f043c3df7df224db8187feb9a4c11edd&oe=608B3987&_nc_sid=7bff83" class="rounded-circle">
+            </img>
+        </div>
+
+        <div class="col-9">
+        <div><h1 class="pt-5" style="font-size: 18px;">{{ $user->username }}</h1></div>
+
+        <div class="d-flex">
+              <div class="pr-4"><strong>3500</strong> posts</div>
+              <div class="pr-4"><strong>5</strong> followers</div>
+              <div class="pr-4"><strong>954</strong> following</div>
+        </div>
+
+        <div class="pt-4 font-weight-bold">pornhub.com</div>
+        <div class="w-100">Arnold Alois Schwarzenegger is an Austrian-American actor, businessman, retired bodybuilder, and former politician who served as the 38th governor of California</div>
+        <div><a href="https://www.instagram.com/xzvirgzdins/" style="color:#0055C5">Click here for nudes</a></div>
+
+        </div>
+   </div>
+
+    <div class="row pt-5">
+
+        <div class="col-4">
+            <img src="https://instagram.frix3-1.fna.fbcdn.net/v/t51.2885-15/e35/c236.0.608.608a/45829553_263923114281467_193156580182097741_n.jpg?tp=1&_nc_ht=instagram.frix3-1.fna.fbcdn.net&_nc_cat=111&_nc_ohc=H4ph_2vt0wkAX9OOZ-M&ccb=7-4&oh=3036550f409341145aef32e897fe0eb6&oe=608862D4&_nc_sid=7bff83 "
+            class="w-100">
+        </div>
+        <div class="col-4">
+            <img src="https://instagram.frix3-1.fna.fbcdn.net/v/t51.2885-15/sh0.08/e35/s640x640/66299373_383232845728981_4726345720830785307_n.jpg?tp=1&_nc_ht=instagram.frix3-1.fna.fbcdn.net&_nc_cat=101&_nc_ohc=h6vXECdztBMAX95AUub&ccb=7-4&oh=0e1232e29bf8f9e43e7a5b002257535b&oe=6088ED74&_nc_sid=7bff83 "
+            class="w-100">
+        </div>
+        <div class="col-4">
+            <img src="https://instagram.frix3-1.fna.fbcdn.net/v/t51.2885-15/sh0.08/e35/s640x640/45893326_1418077944992421_5196083017942228211_n.jpg?tp=1&_nc_ht=instagram.frix3-1.fna.fbcdn.net&_nc_cat=109&_nc_ohc=4pxuC-rUxIMAX8Qce3F&ccb=7-4&oh=ff799bea5167efbf50383a1ff8f994e9&oe=608A8A41&_nc_sid=7bff83 "
+            class="w-100">
+        </div>
+    </div>
+
+</div>
+@endsection
